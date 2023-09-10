8fcad36e7c03420368dac5072c34c5a04c2d0cca
eff3587 <eff3587@rit.edu>
2023-09-09 23:26:17 -0400

    Refetched everything from Cascioli's branch along with my mesh changes.

---
+++ Mesh.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <wrl/client.h>
+#include "Vertex.h"
+#include <d3d11.h>
+
+class Mesh {
+public:
+	Mesh(
+		Vertex* verticies, 
+		unsigned int verticiesCount, 
+		unsigned int* indices, 
+		unsigned int indicesCount, 
+		Microsoft::WRL::ComPtr<ID3D11Device> device, 
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext
+	);
+	~Mesh();
+	Microsoft::WRL::ComPtr<ID3D11Buffer>  GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer>  GetIndexBuffer();
+	int GetIndexCount();
+	void Draw();
+
+private:
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Device> device;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext;
+	Vertex* verticies;
+	unsigned int verticiesCount;
+	unsigned int* indices;
+	unsigned int indicesCount;
+	int indexBufferCount;
+};
