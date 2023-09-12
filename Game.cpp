f3b9360be3b847e8ffe300580aab1b9681dfeaf8
eff3587 <eff3587@rit.edu>
2023-09-12 19:11:34 -0400

    Added window width and height. Revised my method to print a char*

--- Game.cpp
+++ Game.cpp
@@ -280,10 +280,10 @@ void Game::Update(float deltaTime, float totalTime)
 {
 
 	FeedInputsToImGui(deltaTime);
-	
-	char framerateString[100];
-	snprintf(framerateString, sizeof framerateString, "%f", ImGui::GetIO().Framerate);
-	ImGui::Text(framerateString);
+	ImGui::Text("Framerate: %f", ImGui::GetIO().Framerate);
+	ImGui::Text("Window Width: %lu", windowWidth);
+	ImGui::Text("Window Height: %lu", windowHeight);
+
 
 	// Example input checking: Quit if the escape key is pressed
 	if (Input::GetInstance().KeyDown(VK_ESCAPE))
