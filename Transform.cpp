b19cea3b24658c069c8032d6575ab5139647871a
eff3587 <eff3587@MSS2150-32>
2023-09-19 22:21:58 -0400

    Implemented a couple setters in Transform.cpp with overloads

--- Transform.cpp
+++ Transform.cpp
@@ -10,3 +10,34 @@ Transform::Transform() :
 	XMStoreFloat4x4(&worldInverseTranspose, XMMatrixIdentity());
 	matrixDirty = false;
 }
+
+void Transform::SetPosition(float x, float y, float z)
+{
+	position.x = x;
+	position.y = y;
+	position.z = z;
+}
+
+void Transform::SetPosition(DirectX::XMFLOAT3 position)
+{
+	SetPosition(position.x, position.y, position.z);
+}
+
+void Transform::SetRotation(float pitch, float yaw, float roll)
+{
+	pitchYawRoll.x = pitch;
+	pitchYawRoll.y = yaw;
+	pitchYawRoll.z = roll;
+}
+
+void Transform::SetRotation(DirectX::XMFLOAT3 rotation)
+{
+}
+
+void Transform::SetScale(float x, float y, float z)
+{
+}
+
+void Transform::SetScale(DirectX::XMFLOAT3 scale)
+{
+}
--- Transform.h
+++ Transform.h
@@ -5,6 +5,12 @@ class Transform
 {
 public:
 	Transform();
+	void SetPosition(float x, float y, float z);
+	void SetPosition(DirectX::XMFLOAT3 position);
+	void SetRotation(float pitch, float yaw, float roll);
+	void SetRotation(DirectX::XMFLOAT3 rotation);
+	void SetScale(float x, float y, float z);
+	void SetScale(DirectX::XMFLOAT3 scale);
 
 private:
 	DirectX::XMFLOAT3 position;
