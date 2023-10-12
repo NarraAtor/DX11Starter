557b7e2674424482eb99ae98da6fc7d0d7da502b
NarraAtor <eff3587@rit.edu>
2023-10-12 13:31:19 -0400

    Revised Mesh constructor.

--- Mesh.h
+++ Mesh.h
@@ -16,7 +16,9 @@ public:
 	);
 
 	Mesh(
-		const char* model
+		const char* model,
+		Microsoft::WRL::ComPtr<ID3D11Device> device,
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext
 	);
 
 	~Mesh();
