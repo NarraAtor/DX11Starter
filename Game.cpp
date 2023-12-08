85fc049ec647aea162a16c7c03eeca01b3c9567c
NarraAtor <eff3587@rit.edu>
2023-12-08 10:28:37 -0500

    I'm getting the result in the shadow's depth buffer that i want

--- Game.cpp
+++ Game.cpp
@@ -291,11 +291,11 @@ void Game::Init()
 		&srvDesc,
 		shadowSRV.GetAddressOf());
 	
-	int offset = 50;
+	int offset = 80;
 	lightViewMatrix = XMMatrixLookToLH(
-		XMVectorSet(-directionalLight2.Direction.x * offset, -directionalLight1.Direction.y * offset, -directionalLight1.Direction.z * offset, 1.0f), // Position: "Backing up" 20 units from origin
+		XMVectorSet(5, -directionalLight2.Direction.y * offset, -directionalLight2.Direction.z * offset, 1.0f), // Position: "Backing up" 20 units from origin
 		//XMVectorSet(0.0f, 20.0f, 10.0f, 1.0f),
-		XMVectorSet(directionalLight2.Direction.x, directionalLight1.Direction.y , directionalLight1.Direction.z, 1.0f), // Direction: light's direction
+		XMVectorSet(directionalLight2.Direction.x, directionalLight2.Direction.y , directionalLight2.Direction.z, 1.0f), // Direction: light's direction
 		XMVectorSet(0, 1, 0, 0)); // Up: World up vector (Y axis)
 
 	float lightProjectionSize = 15.0f; // Tweak for your scene!
@@ -432,7 +432,7 @@ void Game::CreateGeometry()
 	gameEntities.push_back(GameEntity(quad, materials[1]));
 	gameEntities[3].GetTransform()->MoveAbsolute(XMFLOAT3(15.0f, 0.0f, 10.0f));
 	gameEntities[3].GetTransform()->Scale(XMFLOAT3(20.0f, 20.0f, 20.0f));
-	gameEntities[3].GetTransform()->Rotate(25.0f, 0.0f, 0.0f);
+	gameEntities[3].GetTransform()->Rotate(30.0f, 0.0f, 0.0f);
 
 
 
